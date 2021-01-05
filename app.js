bc4f8d44072667810f91ea1ed02f28d78bb263da
Neil Foster <neilfoster81@gmail.com>
2021-01-05 23:30:45 +0000

    decalred intial consts for game

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const trump = document.querySelector('.trump')
+    const gameDisplay = document.querySelector('.game-container')
+    const ground = document.querySelector('.ground')
+
+})
